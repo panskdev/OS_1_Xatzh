821610dd73a6eb29aa0ac803e2b579db32dd130c
panskdev_LAPTOP <panagiotis.skarpathakis@gmail.com>
2025-12-15 20:48:59 +0200

    Removed CALL_SEM and CALL_VOID_SEM from debug to chat.h

--- include/debug.h
+++ include/debug.h
@@ -7,20 +7,4 @@
 #define errorMain 1
 #define TEST printf("PASSED %d IN %s\n", __LINE__, __FILE__); if(getchar());
 
-#define	CALL_VOID_SEM(call) {	\
-	int code = call;	\
-	if(code < 0) {	\
-		fprintf(stderr, "Something went wrong at %d in %s (%s) : %s\n", __LINE__, __func__, __FILE__, strerror(errno));	\
-		return;		\
-	}	\
-}
-
-#define	CALL_SEM(call, error) {	\
-	int code = call;	\
-	if(code < 0) {	\
-		fprintf(stderr, "Something went wrong at %d in %s (%s) : %s\n", __LINE__, __func__, __FILE__, strerror(errno));	\
-		return error;		\
-	}	\
-}
-
 #endif
